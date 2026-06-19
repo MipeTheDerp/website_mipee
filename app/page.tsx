4af4769bddea31a95d4471a85eaa6b99d7ca797e
mipe <matz.overby@gmail.com>
2026-06-19 16:27:45 +0200

    Endret tittel på nettsiden

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
         {/* Logo area */}
         <div className="text-2xl font-blod tracking-tight">
-          MySite<span className="texr-blue-400">.</span>
+          Mipee<span className="texr-blue-400"></span>
         </div>
 
         {/* Links area */}
